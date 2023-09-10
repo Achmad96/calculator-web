e6731b412e589f537570f7d7c659812fa8dedbfc
Achmad96 <totsukamail3278@gmail.com>
2023-09-10 16:16:45 +0700

    update project

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import "./App.css";
-import { useState } from "react";
+import { useState, useRef } from "react";
 import Keypad from "./components/keypad";
 import NumberFormat from "./utils/NumberFormat";
+import { motion } from "framer-motion";
 
 function App() {
   const [param, setParam] = useState("");
+  const [selected, setSelected] = useState({ name: "tes" });
   const [keypads] = useState(["Del", "(", ")", "⇦", "+", "-", "X", "/", 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, ".", "="]);
+  const ref = useRef(null);
 
   const handleBtn = v => {
     if (v === "Del") {
@@ -29,7 +32,18 @@ function App() {
         <div className="keypads">
           {keypads.map((v, i) => (
             <div key={v}>
-              <Keypad className={`keypad-${i}`} click={e => handleBtn(e.target.value)} value={v} />
+              <motion.div
+                ref={ref}
+                animate={{ scale: selected[v] ? 1.05 : 1 }}
+                transition={{ duration: 0.3 }}
+                onClick={() => {
+                  setSelected({ [v]: i });
+                  setTimeout(() => {
+                    setSelected({ [v]: undefined });
+                  }, 500);
+                }}>
+                <Keypad className={`keypad-${i}`} click={e => handleBtn(e.target.value)} value={v} />
+              </motion.div>
             </div>
           ))}
         </div>
