82a811bb84510fdd29de286b18f2d25b392d6e2f
Achmad96 <totsukamail3278@gmail.com>
2024-02-25 18:20:41 +0700

    modified files

--- src/components/keypad.jsx
+++ src/components/keypad.jsx
@@ -1,41 +1,41 @@
 const Keypad = props => {
-  const { className, state, value } = props;
-  const [param, setParam] = state;
-  const handleBtn = key => {
-    if (param.length > 0 && isNaN(parseFloat(key)) && param[param.length - 1] === key) {
-      return;
-    } else if (param.length > 0 && isNaN(parseFloat(param[param.length - 1])) && !param.endsWith(")") && isNaN(key) && key !== "backspace" && key !== "=" && key !== "clear") {
-      return;
-    } else if (param.length === 0 && isNaN(parseFloat(key)) && key !== "(") {
-      return;
-    }
+    const { className, state, value } = props;
+    const [param, setParam] = state;
+    const handleBtn = key => {
+        if (param.length > 0 && isNaN(parseFloat(key)) && param[param.length - 1] === key) {
+            return;
+        } else if (param.length > 0 && isNaN(parseFloat(param[param.length - 1])) && !param.endsWith(")") && isNaN(key) && key !== "backspace" && key !== "=" && key !== "clear") {
+            return;
+        } else if (param.length === 0 && isNaN(parseFloat(key)) && key !== "(") {
+            return;
+        }
 
-    switch (key.toLowerCase()) {
-      case "clear":
-        setParam("");
-        break;
-      case "backspace":
-        if (!param) return;
-        setParam(prevParam => prevParam.toString().substring(0, param.length - 1));
-        break;
-      case "=":
-        if (!param) return;
-        try {
-          const result = eval(param.replaceAll("x", "*"));
-          setParam(result);
-        } catch (e) {}
-        break;
-      default:
-        setParam(prev => prev + key);
-        break;
-    }
-  };
+        switch (key.toLowerCase()) {
+            case "clear":
+                setParam("");
+                break;
+            case "backspace":
+                if (!param) return;
+                setParam(prevParam => prevParam.toString().substring(0, param.length - 1));
+                break;
+            case "=":
+                if (!param) return;
+                try {
+                    const result = eval(param.replaceAll("x", "*"));
+                    setParam(result);
+                } catch (e) {}
+                break;
+            default:
+                setParam(prev => prev + key);
+                break;
+        }
+    };
 
-  return (
-    <button onClick={e => handleBtn(e.target.value.toLowerCase())} className={className} value={value.toLowerCase()}>
-      {value}
-    </button>
-  );
+    return (
+        <button onClick={e => handleBtn(e.target.value.toLowerCase())} className={className} value={value.toLowerCase()}>
+            {value}
+        </button>
+    );
 };
 
 export default Keypad;
